ae8586cbec3c859c5ffcdf10e6f6c677c6a4ee19
Alfonce Yano <ogongoyanojnr@gmail.com>
2025-11-05 14:40:58 +0300

    🍵🍵🍵using reject andx catch

--- promises/implement.js
+++ promises/implement.js
@@ -1,37 +1,69 @@
 // implementation using promises
 function walkdog() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(`You walk the 🐕‍🦺🐕`);
-    }, 1500);
-  });
+  const dogWalked = true;
+  if (dogWalked) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(`You walk the 🐕‍🦺🐕`);
+      }, 1500);
+    });
+  } else {
+    reject("You did not walk the dog");
+  }
 }
 
 function clearKitchen() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(`You clear the Kitchen 🔪`);
-    }, 2500);
-  });
+  const kitchenCleared = true;
+  if (!kitchenCleared) {
+    reject("You did not clear the kitchen");
+  } else {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(`You clear the Kitchen 🔪`);
+      }, 2500);
+    });
+  }
 }
 
+// function takeOutTrash() {
+//   const trashEmpty = true;
+//   if (trashEmpty){
+//     return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve(` You take out the trash 🚮`);
+//     }, 500);
+//   });
+//   }else{
+//     reject('You did not clear the trash');
+//   }
+
+// }
+
 function takeOutTrash() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(` You take out the trash 🚮`);
-    }, 500);
-  });
+  const trashEmpty = true;
+  return (
+    trashEmpty &&
+    new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve("You take out the trash 🚮");
+      }, 500) || Promise.reject("You did not clear the trash");
+    })
+  );
 }
 
 // now use method chaining
 
-walkdog().then((value) => {
-  console.log(value);
-  return clearKitchen().then((value) => {
+walkdog()
+  .then((value) => {
     console.log(value);
-    return takeOutTrash().then((value) => {
+    return clearKitchen().then((value) => {
       console.log(value);
-      console.log(`You finished all the chores`);
+      return takeOutTrash().then((value) => {
+        console.log(value);
+        console.log(`You finished all the chores`);
+      });
     });
-  });
-});
+  })
+  .catch((error) => console.error(error));
+
+// learn:Use Ternary Operator
